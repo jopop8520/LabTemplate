3d5d843d9cff4633249588a27cf5174f4e19c55c
Lab <Lab@10.9.26.135>
2016-10-27 10:11:22 +0700

    Template version 1.00

---
+++ src/ReadInput.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class ReadInput {
+
+	public static void main(String[] args) throws IOException {
+		Reader.init(new FileInputStream(new File("data/input.txt")));
+		Reader.setDelimeter("\t,");
+		
+		String s;
+		s = Reader.next();
+		System.out.println(s);
+		
+	}
+
+}
